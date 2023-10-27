1c54e3455e385bc1a05f78316de1855eb087ab13
Sobir <sobirjon001@gmail.com>
2023-10-27 01:08:02 -0500

    fixed issues Jenkins file

--- src/test/java/utils/Config.java
+++ src/test/java/utils/Config.java
@@ -2,12 +2,25 @@ package utils;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Config {
 
   private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+  private static final Map<String, String> envValues = new HashMap<>();
+
+  static {
+    String[] lines = dotenv.get("ENV").split("\\n");
+    for (String line : lines) {
+      line = line.replaceAll("\\s", "");
+      if (!line.startsWith("#") && line.contains("=")) envValues.put(line.split("=")[0], line.split("=")[1]);
+    }
+  }
 
   public static String getEnv(String key) {
-    return dotenv.get(key);
+    return !Objects.equals(dotenv.get(key), null) ? dotenv.get(key) : envValues.get(key);
   }
 
 }
