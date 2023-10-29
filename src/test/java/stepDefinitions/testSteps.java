fdb345e3041f1783bdbb879c5ec29dfcdc8a17b9
Sobir <sobirjon001@gmail.com>
2023-10-29 03:06:18 -0500

    Added assertions

--- src/test/java/stepDefinitions/testSteps.java
+++ src/test/java/stepDefinitions/testSteps.java
@@ -1,16 +1,19 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Given;
+import org.junit.Assert;
 import utils.Config;
 
 public class testSteps {
   @Given("I print env variable {string}")
   public void iPrintEnvVariable(String envVariableKey) {
     System.out.println(envVariableKey + " = " + Config.getEnv(envVariableKey));
+    Assert.assertTrue(true);
   }
 
-    @Given("I print all env variables")
-    public void iPrintAllEnvVariables() {
-      System.out.println(Config.getEnvValues());
-    }
+  @Given("I print all env variables")
+  public void iPrintAllEnvVariables() {
+    System.out.println(Config.getEnvValues());
+    Assert.assertTrue(true);
+  }
 }
