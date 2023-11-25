85b5c8560734f28d00ea71af48aa10ab2214c0dd
Sobir <sobirjon001@gmail.com>
2023-11-24 21:45:11 -0600

    Jenkinsfile fix

--- src/test/java/utils/Helper.java
+++ src/test/java/utils/Helper.java
@@ -3,10 +3,13 @@ package utils;
 public class Helper {
 
   public void openURL(String URL) {
+    System.out.println("Browser opening URL '" + URL + "'");
     Driver.getDriver().get(URL);
   }
 
   public String getTitle() {
-    return Driver.getDriver().getTitle();
+    String title = Driver.getDriver().getTitle();
+    System.out.println("Title is '" + title + "'");
+    return title;
   }
 }
