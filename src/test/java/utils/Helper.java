56603e4177982d911d9669a50e196038010ca038
Sobir <sobirjon001@gmail.com>
2023-11-24 23:18:24 -0600

    Jenkinsfile fix

--- src/test/java/utils/Helper.java
+++ src/test/java/utils/Helper.java
@@ -1,6 +1,15 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class Helper {
+
+  int explicit_wait_minutes = 1;
 
   public void openURL(String URL) {
     System.out.println("Browser opening URL '" + URL + "'");
@@ -8,6 +17,8 @@ public class Helper {
   }
 
   public String getTitle() {
+    Wait<WebDriver> wait = new WebDriverWait(Driver.getDriver(), Duration.ofMinutes(explicit_wait_minutes));
+    wait.until(ExpectedConditions.titleIs("Google"));
     String title = Driver.getDriver().getTitle();
     System.out.println("Title is '" + title + "'");
     return title;
