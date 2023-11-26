35e5500136ed699a74e17f4996d50f818f5c5005
Sobir <sobirjon001@gmail.com>
2023-11-26 14:14:53 -0600

    Updated Jenkinsfile

--- Jenkins/wait-for-grid.sh
+++ Jenkins/wait-for-grid.sh
@@ -2,7 +2,10 @@
 #wait-for-grid.sh team_name
 
 set -e
-url="http://$1-selenium-hud:4444/wd/hub/status"
+
+cmd="$@"
+
+url="http://$TEAM-selenium-hud:4444/wd/hub/status"
 wait_interval_in_seconds=1
 max_wait_time_in_seconds=30
 end_time=$((SECONDS + max_wait_time_in_seconds))
@@ -11,7 +14,7 @@ time_left=$max_wait_time_in_seconds
 while [ $SECONDS -lt $end_time ]; do
   response=$(curl -sL "$url" | jq -r '.value.ready')
   if [ -n "$response" ] && [ "$response" ]; then
-    echo "Selenium Grid for team $1 is up - executing tests"
+    echo "Selenium Grid for team $TEAM is up - executing tests"
     break
   else
     echo "Waiting for Grid for team $1. Sleeping fot $wait_interval_in_seconds second(s). $time_left seconds seconds left until timeout."
@@ -21,6 +24,8 @@ while [ $SECONDS -lt $end_time ]; do
 done
 
 if [ $SECONDS -ge $end_time ]; then
-  echo "Timeout: The Grid for team $1 was not started within $max_wait_time_in_seconds seconds."
+  echo "Timeout: The Grid for team $TEAM was not started within $max_wait_time_in_seconds seconds."
   exit 1
+else
+  exec $cmd
 fi
