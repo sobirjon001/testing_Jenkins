1a4abb59f086855b4b5c0c90a5168bade64c35de
Sobir <sobirjon001@gmail.com>
2023-11-26 14:22:24 -0600

    Updated Jenkinsfile

--- Jenkins/wait-for-grid.sh
+++ Jenkins/wait-for-grid.sh
@@ -5,11 +5,13 @@ set -e
 
 cmd="$@"
 
-url="http://$TEAM-selenium-hud:4444/wd/hub/status"
+url="http://$TEAM-selenium-hub:4444/wd/hub/status"
 wait_interval_in_seconds=1
 max_wait_time_in_seconds=30
 end_time=$((SECONDS + max_wait_time_in_seconds))
 time_left=$max_wait_time_in_seconds
+
+echo "url is $url"
 
 while [ $SECONDS -lt $end_time ]; do
   response=$(curl -sL "$url" | jq -r '.value.ready')
